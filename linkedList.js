90beb76da782e64023fcbec1fa7005fe2159d5bf
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-05-24 21:35:14 -1000

    Just got done with Add

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,49 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+      var head=null;
+      var tail= null;
 
-}
+      function getHead(){
+        return head;
+      }
+      function getTail(){
+        return tail;
+      }
+
+      function add(value){
+        var newNode={
+          value:value,
+          next:null
+        }
+
+        if (head===null ){
+          tail=newNode;
+          head=newNode;
+        }else if(tail.next===null){
+          tail=newNode;
+      }
+        return newNode;
+  };
+
+
+      function remove(number){
+
+      }
+      function get(number){
+
+      }
+      function insert(value,number){
+
+      }
+
+
+  return{
+    getHead:getHead,
+    getTail:getTail,
+    add:add,
+    remove:remove,
+    get:get,
+    insert:insert,
+  }
+};
